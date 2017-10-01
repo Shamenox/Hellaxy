a9bed4e8a9946261dfec10dae4c509548e9ef1a9
Shamenox <Shamenox@gmail.com>
2017-10-01 19:20:12 +0200

    Ressourcen wieder importiert

--- ress.js
+++ ress.js
@@ -1,9 +1,51 @@
 images = {
 	cursor : "sys",
 	arrow : "sys",
-	blackscreen : "sys"
+	blackscreen : "sys",
+	whitescreen : "sys",
+	
+	central : "bgs",
+	omar : "bgs",
+	outer : "bgs",
+	testmap : "bgs",
+	
+	chestanian_fortress : "planets",
+	imat_chestcolony : "planets",
+	humania : "planets",
+	pontes : "planets",
+	testmoon : "planets",
+	
+	beam_ophianian : "projectiles",
+	explosion : "projectiles",
+	shot_light_1 : "projectiles",
+	shot_medium_tripple : "projectiles",
+	testarrow : "projectiles",
+	
+	chestanian_colonizer : "ships",
+	chestanian_fighter : "ships",
+	chestanian_quintalfighter : "ships",
+	cylon_basestar : "ships",
+	helon_baseship_mkii : "ships",
+	helon_raider_mkiii : "ships",
+	humanian_protobaseship_helonia : "ships",
+	humanian_shuttle : "ships",
+	humanian_satalite : "ships",
+	none_fatman : "ships",
+	none_testarrow : "ships",
+	ophianic_annector : "ships",
+	ophianic_chunk : "ships",
+	ophianic_chunk : "ships",
+	qubanic_colonizer : "ships",
+	republic_hq : "ships",
+	samus_ship : "ships",
 }
 
 function loadAudio(){
 	Helon.ress.audio.theme1 = new Audio("ress/audio/theme1.mp3");
+	Helon.ress.audio.shot_1 = new Audio("ress/audio/shot_1.mp3");
+	Helon.ress.audio.hit_1 = new Audio("ress/audio/hit_1.mp3");
+	Helon.ress.audio.bounce_1 = new Audio("ress/audio/bounce_1.mp3");
+	Helon.ress.audio.engine1 = new Audio("ress/audio/engine1.mp3");
+	Helon.ress.audio.theme1 = new Audio("ress/audio/theme1.mp3");
+	Helon.ress.audio.explosion1 = new Audio("ress/audio/bomb 3-soundbible.com-1260663209.wav");
 }
